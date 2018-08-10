23efd034a4ac9ca6b45ed6a454c584a5468c6e8b
Kuni <Kuni@Kuni-PC>
2018-08-11 00:24:25 +0530

    Step 02 Completed
    Create LoginServlet again
    Redirect to a view - JSP

--- JavaWebApplication/src/main/java/com/webapp/servlet/LoginServlet.java
+++ JavaWebApplication/src/main/java/com/webapp/servlet/LoginServlet.java
@@ -1,9 +1,8 @@
 package com.webapp.servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.Date;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -13,14 +12,14 @@ import javax.servlet.http.HttpServletResponse;
 public class LoginServlet extends HttpServlet{
 	
 	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException{
-		PrintWriter out = response.getWriter();
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException{
+		/*PrintWriter out = response.getWriter();
 		out.append("<html>");
 		out.append("<title>My First Web Application</title>");
 		out.append("<head></head>");
 		out.append("<body> <h1>Yahoo!!!!!!</h1> </ br> <h2> My First Servlet Runnning @ "+ new Date() +" </h2> </body>");
-		out.append( "</html>");
-		
+		out.append( "</html>");*/
+		request.getRequestDispatcher("/WEB-INF/views/login.jsp").forward(request, response);
 	}
 
 }
